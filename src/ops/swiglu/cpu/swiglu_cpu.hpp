6ea78401c0c833714c6a43c6e6a459d56d1d2c46
AthBe1337 <prayer@athbe.cn>
2025-12-30 01:40:46 +0800

    feat(swiglu): Remove default values for gate_scale, up_scale, and output_scale in swiglu function

--- src/ops/swiglu/cpu/swiglu_cpu.hpp
+++ src/ops/swiglu/cpu/swiglu_cpu.hpp
@@ -5,5 +5,5 @@
 
 namespace llaisys::ops::cpu {
 void swiglu(std::byte *out, const std::byte *gate, const std::byte *up, llaisysDataType_t type, size_t numel, 
-            float gate_scale = 1.0f, float up_scale = 1.0f, float output_scale = 1.0f);
+            float gate_scale, float up_scale, float output_scale);
 }
